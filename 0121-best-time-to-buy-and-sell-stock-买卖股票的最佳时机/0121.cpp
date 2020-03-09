a27d779c9ca3601a9db8454bbf260f1675e4aea4
jendon <jendonyuen@qq.com>
2020-03-09 20:59:58 +0800

    update 0121 solution

--- 0121-best-time-to-buy-and-sell-stock-买卖股票的最佳时机/0121.cpp
+++ 0121-best-time-to-buy-and-sell-stock-买卖股票的最佳时机/0121.cpp
@@ -3,17 +3,10 @@ public:
     int maxProfit(vector<int>& prices) {
         if (prices.size() < 2)return 0;
         int buy = prices[0];
-        int sell = prices[1];
         int price = 0;
         for(int i = 1; i != prices.size(); ++i) {
-            if (prices[i] < buy) {
-                buy = prices[i];
-                sell = prices[i];
-            }
-            if (prices[i] > sell) {
-                sell = prices[i];
-            }
-            if (sell - buy > price) price = sell - buy;
+            buy = prices[i] < buy ? prices[i] : buy;
+            price = prices[i] - buy > price ? prices[i] - buy : price;
         }
         return price;
     }
