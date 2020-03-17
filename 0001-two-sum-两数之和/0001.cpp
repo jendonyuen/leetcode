449a513ad33366b0508bf7f3cab9fb19ce47e249
jendon <jendonyuen@qq.com>
2020-03-18 00:21:48 +0800

    update 0001 solution

--- 0001-two-sum-两数之和/0001.cpp
+++ 0001-two-sum-两数之和/0001.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> v;
-        for (int i = 0; i != nums.size(); ++i) {
-            int b = target - nums[i];
-                for (int j = i + 1; j!=nums.size(); ++j) {
-                    if (nums[j] == b) {
-                        v.push_back(i);
-                        v.push_back(j);
-                        return v; 
-                    }
-                }
+        unordered_map<int, int> table;
+        for (int i = 0; i != nums.size(); i++) {
+            if (table.find(target - nums[i]) != table.end()) {
+                return {table[target - nums[i]], i};
+            } else {
+                table[nums[i]] = i;
+            }
         }
-        return v;
+        return {};
     }
 };
