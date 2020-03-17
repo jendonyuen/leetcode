eeeac74f92b075e0b26f16bf08901f95a241afa7
jendon <jendonyuen@qq.com>
2020-03-17 22:18:39 +0800

    update 0118 solution

--- 0118-pascals-triangle-杨辉三角/0118.cpp
+++ 0118-pascals-triangle-杨辉三角/0118.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> v;
-        if (numRows == 0) return v;
-
-        vector<int> t = {1};
-        v.push_back(t);
-        if (numRows == 1) return v;
-
-        t.push_back(1);
-        v.push_back(t);
-        if (numRows == 2) return v;
+        vector<vector<int>> v(numRows);
         
-        for (int i = 3; i <= numRows; ++i) {
-            t = {1};
-            for(int p = 1; p != i - 1; ++p) {
-                t.push_back(v[i-2][p-1] + v[i-2][p]);
+        for (int i = 0; i < numRows; ++i) {
+            v[i] = vector<int>(i+1, 1);
+            for(int p = 1; p <= i - 1; ++p) {
+                v[i][p] = v[i-1][p-1] + v[i-1][p];
             }
-            t.push_back(1);
-            v.push_back(t);
         }
 
         return v;
