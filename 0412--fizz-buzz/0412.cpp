c0c23c46ea97c63452bd26bd1a0cc758491c3a93
jendon <jendonyuen@qq.com>
2020-03-22 23:57:22 +0800

    add 0412 solution

---
+++ 0412--fizz-buzz/0412.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> v;
+        for (int i = 1; i != n + 1; ++i) {
+            string s = "";
+            if (i % 3 == 0) s += "Fizz";
+            if (i % 5 == 0) s += "Buzz";
+            if (s.size() == 0) s = to_string(i);
+            v.push_back(s);
+        }
+        return v;
+    }
+};
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ My leetcode solutions.
 #### Statistics
 |2020.03.22 |   |  
 |-----------|---|
-|C++        |66 |  
+|C++        |67 |  
 |Python3    |36 |  
 |JavaScript |3  |  
 |Java       |2  |  
