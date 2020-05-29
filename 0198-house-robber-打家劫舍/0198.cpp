6653d84d0bcd879b116be2fb74b4efca57c29a0e
JendonYuen <jendonyuen@qq.com>
2020-05-29 17:26:21 +0800

    update 0198 cpp solution

--- 0198-house-robber-打家劫舍/0198.cpp
+++ 0198-house-robber-打家劫舍/0198.cpp
@@ -1,3 +1,19 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size() < 1) return 0;
+        if (nums.size() == 1) return nums[0];
+        vector<int> money = {nums[0]};
+        money.push_back(nums[0] > nums[1] ? nums[0] : nums[1]);
+        for (int i = 2; i < nums.size(); ++ i) {
+            int curr = nums[i] + money[i-2];
+            if (money[i-1] > curr) curr = money[i-1];
+            money.push_back(curr);
+        }
+        return money.back();
+    }
+};
+/* 
 // 与面试题 17.16. 按摩师相同
 class Solution {
 public:
@@ -19,3 +35,4 @@ public:
         return times.back();
     }
 };
+*/
