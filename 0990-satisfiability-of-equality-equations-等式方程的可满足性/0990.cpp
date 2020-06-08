89b8d2c9d4743be6ed93689733415c1308bed2bc
JendonYuen <jendonyuen@qq.com>
2020-06-08 18:28:17 +0800

    update 0990 solution

--- 0990-satisfiability-of-equality-equations-等式方程的可满足性/0990.cpp
+++ 0990-satisfiability-of-equality-equations-等式方程的可满足性/0990.cpp
@@ -1,3 +1,51 @@
+// 并查集 按秩合并
+class Solution {
+    unordered_map<char, char> us;
+    unordered_map<char, int> rank;
+    char findRoot(char c) {
+        if (us.find(c) == us.end() || us[c] == c) {
+            return c;
+        }
+        return findRoot(us[c]);
+    }
+    int getRank(char root) {
+        if(rank.find(root) == rank.end()) {
+            return 0;
+        } 
+        return rank[root];
+    }
+public:
+    bool equationsPossible(vector<string>& equations) {
+        for (auto &eq: equations) {
+            if (eq[1] == '=') {
+                char rootLeft = findRoot(eq[0]);
+                char rootRight = findRoot(eq[3]);
+                int rankLeft = getRank(rootLeft);
+                int rankRight = getRank(rootRight);
+                // 按秩合并
+                if (rankLeft > rankRight) {
+                    us[rootRight] = rootLeft;
+                } else if (rankLeft < rankRight) {
+                    us[rootLeft] = rootRight;
+                } else {
+                    us[rootLeft] = rootRight;
+                    rank[rootRight]++;
+                }
+            }
+        }
+        for (auto &eq: equations) {
+            if (eq[1] == '!') {
+                if (findRoot(eq[0]) == findRoot(eq[3])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+/*
+// 并查集
 class Solution {
     unordered_map<char, char> us;
     char findRoot(char c) {
@@ -10,14 +58,11 @@ public:
     bool equationsPossible(vector<string>& equations) {
         for (auto &eq: equations) {
             if (eq[1] == '=') {
-                // cout << eq[0] << "==" << eq[3] << endl;
                 us[findRoot(eq[0])] = findRoot(eq[3]);
             }
         }
         for (auto &eq: equations) {
             if (eq[1] == '!') {
-                // cout << eq[0] << "!=" << eq[3] << endl;
-                // cout << findRoot(eq[0]) << "?" << findRoot(eq[3]) << endl;
                 if (findRoot(eq[0]) == findRoot(eq[3])) {
                     return false;
                 }
@@ -26,3 +71,4 @@ public:
         return true;
     }
 };
+*/
