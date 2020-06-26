2a0d754e301902c301972e798dd710b43d61a3c5
jendon <jendonyuen@qq.com>
2020-06-26 13:34:04 +0800

    udpate 0004 solution

--- 0004-median-of-two-sorted-arrays-寻找两个正序数组的中位数/0004.cpp
+++ 0004-median-of-two-sorted-arrays-寻找两个正序数组的中位数/0004.cpp
@@ -1,3 +1,54 @@
+/*
+划分法 时间复杂度:log(min(m, n))
+even:
+len(left_part) == len(right_part)
+max(left_part) <= min(right_part)
+median = (max(left_part) + min(right_part)) / 2
+odd:
+len(left_part) == len(right_part) + 1
+max(left_part) <= min(right_part)
+median = max(left_part)
+*/
+
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        // 确保第一个数组长度小于第二个数组
+        if (nums1.size() > nums2.size()) return findMedianSortedArrays(nums2, nums1);
+        
+        int size1 = nums1.size();
+        int size2 = nums2.size();
+
+        int beg = 0, end = size1, ansi = -1;
+        // median1: 前部分的最大值
+        // median2: 后部分的最小值
+        int median1 = 0, median2 = 0;
+
+        while(beg <= end) {
+            int i1 = (beg+end) / 2;
+            int i2 = (size1+size2+1) / 2 - i1;
+
+            // 计算分割线两侧的值, 注意边界判断
+            int nums1Left = (i1 == 0 ? INT_MIN: nums1[i1-1]);
+            int nums1Right = (i1 == size1 ? INT_MAX: nums1[i1]);
+            int nums2Left = (i2 == 0 ? INT_MIN: nums2[i2-1]);
+            int nums2Right = (i2 == size2 ? INT_MAX: nums2[i2]);
+
+            if (nums1Left <= nums2Right) {
+                ansi = i1;
+                median1 = max(nums1Left, nums2Left);
+                median2 = min(nums1Right, nums2Right);
+                beg = i1 + 1;             
+            } else {
+                end = i1 - 1;
+            }
+        }
+
+        return (size1+size2) & 1 ? median1 : (median1+median2) / 2.0;
+    }
+};
+
 
 /* 枚举法 O((m+n)/2)
 class Solution {
