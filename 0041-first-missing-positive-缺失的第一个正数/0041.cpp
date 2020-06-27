4b5edeef044b355a6515a8ac0105c7015f07df29
jendon <jendonyuen@qq.com>
2020-06-27 08:52:27 +0800

    update 0041 solution

--- 0041-first-missing-positive-缺失的第一个正数/0041.cpp
+++ 0041-first-missing-positive-缺失的第一个正数/0041.cpp
@@ -1,3 +1,28 @@
+// 原地hash, 时间复杂度O(n), 空间复杂度O(1)
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        // [1] 将非正数变为n+1
+        int size = nums.size();
+        for (auto &n : nums) {
+            if (n <= 0) n = size + 1;
+        }
+        // [2] 若指向索引在[1, n]范围内, 将索引(i-1)对应位置标记为负数 
+        for (auto &n : nums) {
+            if (abs(n) <= size) {
+                nums[abs(n)-1] = -abs(nums[abs(n)-1]);
+            }
+        }
+        // [3] 返回第一个大于0的值的索引
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] > 0) return i+1;
+        }
+        return size + 1;
+    }
+};
+
+
+/* O(n2)
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
@@ -14,3 +39,4 @@ public:
         return i;
     }
 };
+*/
