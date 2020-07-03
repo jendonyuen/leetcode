3eca92465e612eb8dea8f1350ede0e3e6cf0bd05
JendonYuen <jendonyuen@qq.com>
2020-07-03 11:11:15 +0800

    update 0108 solution

--- 0108-convert-sorted-array-to-binary-search-tree-将有序数组转换为二叉搜索树/0108.cpp
+++ 0108-convert-sorted-array-to-binary-search-tree-将有序数组转换为二叉搜索树/0108.cpp
@@ -7,6 +7,24 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+class Solution {
+    TreeNode* build(vector<int>& nums, int beg, int end) {
+        if (beg > end) return NULL;
+        int mid = (beg + end) >> 1;
+        TreeNode* root = new TreeNode(nums[mid]);
+        root->left = build(nums, beg, mid - 1);
+        root->right = build(nums, mid + 1, end);
+        return root;
+    }
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return build(nums, 0, nums.size() - 1);
+    }
+};
+
+
+/*
 class Solution {
 private:
     TreeNode* helper(vector<int>& nums, int beg, int end) {
@@ -23,3 +41,4 @@ public:
         return helper(nums, 0, nums.size());
     }
 };
+*/
