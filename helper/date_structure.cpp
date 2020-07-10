33623ea1823c7a5751b361e5b91cdde74e56c82c
JendonYuen <jendonyuen@qq.com>
2020-07-10 18:21:03 +0800

    update trie tree

--- helper/date_structure.cpp
+++ helper/date_structure.cpp
@@ -1,7 +1,32 @@
 // 字典树
 class TrieNode {
 public:
-    bool isEnd = false;
+    bool end = false;
+    bool rend = false;
+    // string word; // 可用word替换end
     TrieNode* next[26];
+    // 正序插入
+    void insert(string& s) {
+        TrieNode* node = this;
+        for (auto &c: s) {
+            int i = c - 'a';
+            if (!node->next[i]) {
+                node->next[i] = new TrieNode();
+            }
+            node = node->next[i];
+        }
+        node->end = true;
+    }
+    // 倒序插入
+    void rinsert(string& s) {
+        TrieNode* node = this;
+        for (auto it = s.rbegin(); it != s.rend(); ++it) {
+            int i = *it - 'a';
+            if (!node->next[i]) {
+                node->next[i] = new TrieNode();
+            }
+            node = node->next[i];
+        }
+        node->rend = true;
+    }
 };
-
