f25ffc65453b5fdb9de68eac9b27cd311cf4f6fd
jendon <jendonyuen@qq.com>
2020-07-16 20:20:50 +0800

    update 0347 solution

--- 0347-top-k-frequent-elements-前K个高频元素/0347.cpp
+++ 0347-top-k-frequent-elements-前K个高频元素/0347.cpp
@@ -5,6 +5,30 @@ public:
         for (auto &n : nums) {
             count[n]++;
         }
+        
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+        for (auto it = count.begin(); it != count.end(); ++it) {
+            pq.push(make_pair(it->second, it->first));
+            if (pq.size() > k) pq.pop();
+        }
+
+        vector<int> res;
+        for (int i = 0; i < k; ++i) {
+            res.push_back(pq.top().second);
+            pq.pop();
+        }
+        return res;
+    }
+};
+
+/*
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> count;
+        for (auto &n : nums) {
+            count[n]++;
+        }
         multiset<pair<int, int>> ms;
         for (auto it = count.begin(); it != count.end(); ++it) {
             ms.insert(make_pair((*it).second, (*it).first));
@@ -18,3 +42,4 @@ public:
         return res;
     }
 };
+*/
