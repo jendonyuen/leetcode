9897d59a86365ebb1c5d01a5e2df9ce0f24687ed
JendonYuen <jendonyuen@qq.com>
2020-08-11 12:20:55 +0800

    add 0215 quick select(sort) solution

--- 0215-kth-largest-element-in-an-array-数组中的第K个最大元素/0215.cpp
+++ 0215-kth-largest-element-in-an-array-数组中的第K个最大元素/0215.cpp
@@ -1,3 +1,43 @@
+// 2020-08-11, 时间复杂度O(n)
+// 基于快排的选择方法
+// 快排可参考912题
+// ref: https://leetcode-cn.com/problems/kth-largest-element-in-an-array/solution/shu-zu-zhong-de-di-kge-zui-da-yuan-su-by-leetcode-/
+class Solution {
+    int partition(vector<int>& nums, int l, int r) {
+        auto pivot = nums[r];
+        int i = l;
+        for (int j = l; j < r; ++j) {
+            if (nums[j] > pivot) {
+                swap(nums[i], nums[j]);
+                i++;
+            }
+        }
+        swap(nums[i], nums[r]);
+        return i;
+    }
+
+    int randomized_parition(vector<int>& nums, int l, int r) {
+        int i = rand() % (r - l + 1) + l;
+        swap(nums[i], nums[r]);
+        return partition(nums, l, r);
+    }
+
+    int randomized_quickselect(vector<int>& nums, int l, int r, int k) {
+        int pos = randomized_parition(nums, l, r);
+        // pos左边的必定比nums[pos]小, 右边的必定比nums[pos]大
+        if (pos == k - 1) return pos;
+        else if (pos < k) return randomized_quickselect(nums, pos + 1, r, k);
+        else return randomized_quickselect(nums, l, pos - 1, k);
+    }
+
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int pos = randomized_quickselect(nums, 0, nums.size() - 1, k);
+        return nums[pos];
+    }
+};
+
+
 // 小顶堆
 class Solution {
 public:
