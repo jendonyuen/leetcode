01b6aeb7e3b5020d08cec22201d0939956079d4f
JendonYuen <jendonyuen@qq.com>
2020-08-12 13:03:13 +0800

    update 1452 solution

--- 1452-people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list-收藏清单/1452.cpp
+++ 1452-people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list-收藏清单/1452.cpp
@@ -1,16 +1,17 @@
 /*
-
+搜索引擎一般会建立倒排索引: 记录每个关键词在哪个文档中出现。
+本题用HashMap记录每个公司在哪些清单中出现, 比如:
 假设favoriteCompanies中的第1个清单为[a, b, c]
-第一次用HashMap遍历得知
-"a"出现在第1, 2, 3清单
-"b"出现在第1, 3, 5清单
+第一次遍历得知
+"a"出现在第1, 2, 4清单
+"b"出现在第1, 4, 5清单
 "c"出现在第1, 3, 4清单
-显然易见, 第1个清单的每家公司都在第3个清单中出现过，那么第1个清单必为第3个清单的子集;
+显而易见, 第1个清单的每个名字, 除了在本清单中都出现了, 也都在第4个清单中出现，那么根据题意(没有重复清单)第1个清单必为第4个清单的子集;
 
 所以第二次遍历统计每个清单中，其他清单下标出现的次数，如果出现次数==该清单的size, 则该清单必为其子集。
 
-题解链接：https://leetcode-cn.com/problems/people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list/solution/c-hen-hao-li-jie-de-hashmap-tong-ji-gong-si-ming-c/
 
+我的题解链接：https://leetcode-cn.com/problems/people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list/solution/c-hen-hao-li-jie-de-hashmap-tong-ji-gong-si-ming-c/
 */
 
 
